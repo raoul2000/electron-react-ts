8af47e17c4a6b30b0abec25dbcbcecf3e23106ca
raoul2000 <manu34@free.Fr>
2021-02-02 12:24:54 +0100

    main article style

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -52,15 +52,22 @@ const App = () => {
                 </div>
                 <div className="column-3">
                     <ScrollPanel style={{ width: '100%', height: '100%' }}>
-                        <ul>
-                            {
-                                sources.map(source => (
-                                    <li>
-                                        <div className="source-name truncate-text">{source.name}</div>
-                                    </li>
-                                ))
-                            }
-                        </ul>                        </ScrollPanel>
+                        <article>
+                            <header>
+                                <h1>This is the main Title</h1>
+                            </header>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates corrupti deserunt eius aperiam, ipsa dolorum aspernatur voluptatum ducimus, in eos eum nobis ipsam! Aut, error. Iste ipsa quidem iusto ipsum?
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates corrupti deserunt eius aperiam, ipsa dolorum aspernatur voluptatum ducimus, in eos eum nobis ipsam! Aut, error. Iste ipsa quidem iusto ipsum?
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates corrupti deserunt eius aperiam, ipsa dolorum aspernatur voluptatum ducimus, in eos eum nobis ipsam! Aut, error. Iste ipsa quidem iusto ipsum?
+                            </p>
+                            <footer>
+                                <p>
+                                    Posté le <time dateTime="2015-05-16 19:00">16 Mai</time> par Lisa.
+                                </p>
+                            </footer>
+                        </article>
+                    </ScrollPanel>
                 </div>
             </main>
             <footer>
